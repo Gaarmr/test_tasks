fec82a694af7644f3bb0d85c0805c4823f6611b3
Daniil Romanshen <vdnv@bk.ru>
2022-05-05 19:30:45 +0600

    gitignore

--- bewise.ai/app/app.py
+++ bewise.ai/app/app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+import requests
 from app.db import database, Questions
 
 
@@ -16,7 +16,7 @@ async def startup():
     if not database.is_connected:
         await database.connect()
     # create a dummy entry
-    await Questions.objects.get_or_create(question="Было два козла, сколько?", answer='Два', created_at='Севодня')
+    await Questions.objects.get_or_create(question="Было три козла, сколько?", answer='Три', created_at='Вчера')
 
 
 @app.on_event("shutdown")
@@ -25,3 +25,10 @@ async def shutdown():
         await database.disconnect()
 
 
+@app.post("/get_question/{question_count}")
+async def get_question(question_count: int):
+    r = requests.get(url='https://jservice.io/api/random?', params=question_count)
+    return await r.text  
+
+    # await Questions.objects.get_or_create(question="Было два козла, сколько?", answer='Два', created_at='Севодня')
+    # return await Questions.objects.fields('id')
