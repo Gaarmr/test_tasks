0fa1b3f9b6dd567f2aaa3a5fba299945a73e2215
Daniil Romanshen <vdnv@bk.ru>
2022-05-20 23:22:41 +0600

    post devices, get devices

--- i-sberg.com/app/main.py
+++ i-sberg.com/app/main.py
@@ -1,6 +1,9 @@
 from collections import Counter
+import secrets
 
 from fastapi import FastAPI
+from fastapi import status
+import random
 
 from app.db import database
 from app.redis import redis
@@ -32,11 +35,40 @@ async def anagrams(request: AnagramQuery):
         return AnagramResponse(is_anagram=False, anagrams_count=anagrams_count)
 
 
-@app.post("/devices/")
+@app.post("/devices/", status_code=status.HTTP_201_CREATED)
 async def create_devices():
-    pass
+    dev_type_list = ['emeter', 'zigbee', 'lora', 'gsm']
+    dev_list = []
+    dev_id_list = []
+
+    for dev in range(10):
+        dev_type = random.choice(dev_type_list)
+        dev_id = secrets.token_hex(6)
+        dev_list.append({'dev_id': dev_id, 'dev_type': dev_type})
+        dev_id_list.append(dev_id)
+
+    async with database.transaction():
+        query = 'INSERT INTO devices (dev_id, dev_type) VALUES (:dev_id, :dev_type)'
+        await database.execute_many(query, dev_list)
+
+        dev_id_rand = random.sample(dev_id_list, 5)
+        dev_id_query = 'SELECT id FROM devices WHERE dev_id = ANY (:dev_ids)'
+        dev_id_db = await database.fetch_all(dev_id_query, {'dev_ids': dev_id_rand})
+
+        endpoint_list = [{'device_id': dev.id, 'comment': 'I was born!'} for dev in dev_id_db]
+        query = 'INSERT INTO  endpoints (device_id, comment) VALUES (:device_id, :comment)'
+        await database.execute_many(query, endpoint_list)
 
 
 @app.get("/devices/")
 async def get_devices():
-    pass
+    devices_query = '''
+        SELECT dev_type, count(*) as dev_count FROM devices 
+        LEFT JOIN endpoints ON devices.id = endpoints.device_id
+        WHERE endpoints.id IS NULL
+        GROUP BY dev_type
+        '''
+
+    result = await database.fetch_all(devices_query)
+    return {i.dev_type: i.dev_count for i in result}
+
